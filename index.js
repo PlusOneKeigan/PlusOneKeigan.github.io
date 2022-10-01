43af2c29183948339d69ef8829fc0d324d2cd75d
PlusOneKeigan <114789900+PlusOneKeigan@users.noreply.github.com>
2022-10-01 00:05:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', (event) => {
 let slideIndex = 0;
 showSlides();
 
@@ -12,3 +13,4 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+})
